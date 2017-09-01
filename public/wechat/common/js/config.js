4c3c73f60055aea6b396327e0f542515ff3ba076
jeff <jeff@jamma.cn>
2017-09-02 07:54:16 +0800

    ApiUri

--- public/wechat/common/js/config.js
+++ public/wechat/common/js/config.js
@@ -1,7 +1,2 @@
-var apiuri = 'http://api.mx.jamma.cn'
-var domain = 'jamma.cn'
-
-var host = document.domain
-if (host.indexOf(domain) >= 0) {
-  document.domain = domain
-}
+var ApiUri
+var apiuri = ApiUri || 'http://api.mx.jamma.cn'
